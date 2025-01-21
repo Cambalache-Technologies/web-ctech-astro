d5c03cbf42e66aa0e7b2a05c51c9d987c61046d6
Leandro Cabranes <cabranesleandro@gmail.com>
2025-01-21 13:52:12 -0300

    contacto

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://c-tech.com.ar'
+  site: 'https://c-tech.com.ar',
+  base: '/web-ctech-astro/',
 });
